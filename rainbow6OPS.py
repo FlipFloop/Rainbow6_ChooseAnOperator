31d142d3fd44e8229d2ece6bc9fcc274c84fb8da
FlipFloop <flipfloopgames@gmail.com>
2018-08-26 15:03:01 -0400

    Create rainbow6OPS.py

---
+++ rainbow6OPS.py
@@ -0,0 +1,292 @@
+# feel free to add entries to the dictionaries!
+
+Ash = {
+    "name": "Ash",
+    "speed": 3,
+    "unit": "FBI",
+    "position": "a",
+    "nickname": "4 speed, pls uninstall"
+}
+Thermite = {
+    "name": "Thermite",
+    "speed": 2,
+    "unit": "FBI",
+    "position": "a",
+    "nickname": "A really big fucking hole comin right up"
+}
+Castle = {
+    "name": "Castle",
+    "speed": 2,
+    "unit": "FBI",
+    "position": "d",
+    "nickname": "why would you play me?"
+}
+Pulse = {
+    "name": "Pulse",
+    "speed": 3,
+    "unit": "FBI",
+    "position": "d",
+    "nickname": "It's Mr. Worldwide"
+}
+Sledge = {
+    "name": "Sledge",
+    "speed": 2,
+    "unit": "SAS",
+    "position": "a",
+    "nickname": "NA"
+}
+Thatcher = {
+    "name": "Thatcher",
+    "speed": 2,
+    "unit": "SAS",
+    "position": "a",
+    "nickname": "fookin laser sights"
+}
+Mute = {
+    "name": "Mute",
+    "speed": 2,
+    "unit": "SAS",
+    "position": "d",
+    "nickname": "shhhhhhh, we're playing the quiet game"
+}
+Smoke = {
+    "name": "Smoke",
+    "speed": 2,
+    "unit": "SAS",
+    "position": "d",
+    "nickname": "What's in the canister?"
+}
+Montagne = {
+    "name": "Montagne",
+    "speed": 1,
+    "unit": "GIGN",
+    "position": "a",
+    "nickname": "Stay behind me"
+}
+Twitch = {
+    "name": "Twitch",
+    "speed": 2,
+    "unit": "GIGN",
+    "position": "a",
+    "nickname": "NA"
+}
+Rook = {
+    "name": "Rook",
+    "speed": 2,
+    "unit": "GIGN",
+    "position": "d",
+    "nickname": "Hi! I'm Lardex, destroyer of buffets (TheRussianBadger)"
+}
+Doc = {
+    "name": "Doc",
+    "speed": 3,
+    "unit": "GIGN",
+    "position": "d",
+    "nickname": "NA"
+}
+IQ = {
+    "name": "IQ",
+    "speed": 3,
+    "unit": "GSG9",
+    "position": "a",
+    "nickname": "NA"
+}
+Blitz = {
+    "name": "Blitz",
+    "speed": 2,
+    "unit": "GSG9",
+    "position": "a",
+    "nickname": "*Rushes you*"
+}
+Jager = {
+    "name": "Jager",
+    "speed": 3,
+    "unit": "GSG9",
+    "position": "d",
+    "nickname": "I'm the defending version of Ash"
+}
+Bandit = {
+    "name": "Bandit",
+    "speed": 3,
+    "unit": "GSG9",
+    "position": "d",
+    "nickname": "Where is my ACOG?"
+}
+Frost = {
+    "name": "Frost",
+    "speed": 2,
+    "unit": "JTF2",
+    "position": "d",
+    "nickname": "4 speed, pls uninstall"
+}
+Buck = {
+    "name": "Buck",
+    "speed": 2,
+    "unit": "JTF2",
+    "position": "a",
+    "nickname": "NA"
+}
+Caveira = {
+    "name": "Caveira",
+    "speed": 3,
+    "unit": "BOPE",
+    "position": "d",
+    "nickname": "I'm terrifying, happy nightmares!"
+}
+Capitao = {
+    "name": "Capitao",
+    "speed": 3,
+    "unit": "BOPE",
+    "position": "a",
+    "nickname": "NA"
+}
+Hibana = {
+    "name": "Hibana",
+    "speed": 3,
+    "unit": "SAT",
+    "position": "a",
+    "nickname": "NA"
+}
+Echo = {
+    "name": "Echo",
+    "speed": 2,
+    "unit": "SAT",
+    "position": "d",
+    "nickname": "NA"
+}
+Glaz = {
+    "name": "Glaz",
+    "speed": 2,
+    "unit": "SPETZNAZ",
+    "position": "a",
+    "nickname": "NA"
+}
+Fuze = {
+    "name": "Fuze",
+    "speed": 2,
+    "unit": "SPETZNAZ",
+    "position": "a",
+    "nickname": "NA"
+}
+Kapkan = {
+    "name": "Kapkan",
+    "speed": 1,
+    "unit": "SPETZNAZ",
+    "position": "d",
+    "nickname": "NA"
+}
+Tachanka = {
+    "name": "Tachanka",
+    "speed": 1,
+    "unit": "SPETZNAZ",
+    "position": "d",
+    "nickname": "NA"
+}
+Jackal = {
+    "name": "Jackal",
+    "speed": 2,
+    "unit": "GEO",
+    "position": "a",
+    "nickname": "NA"
+}
+Mira = {
+    "name": "mira",
+    "speed": 1,
+    "unit": "GEO",
+    "position": "d",
+    "nickname": "I changed the meta. Forever."
+}
+Ela = {
+    "name": "Ela",
+    "speed": 3,
+    "unit": "GROM",
+    "position": "d",
+    "nickname": "NA"
+}
+Zofia = {
+    "name": "Zofia",
+    "speed": 2,
+    "unit": "GROM",
+    "position": "a",
+    "nickname": "NA"
+}
+Ying = {
+    "name": "Ying",
+    "speed": 2,
+    "unit": "SDU",
+    "position": "a",
+    "nickname": "NA"
+}
+Lesion = {
+    "name": "Lesion",
+    "speed": 2,
+    "unit": "SDU",
+    "position": "d",
+    "nickname": "NA"
+}
+Lion = {
+    "name": "Lion",
+    "speed": 2,
+    "unit": "CBRN",
+    "position": "a",
+    "nickname": "NA"
+}
+Finka = {
+    "name": "Finka",
+    "speed": 2,
+    "unit": "CBRN",
+    "position": "a",
+    "nickname": "NA"
+}
+Alibi = {
+    "name": "Alibi",
+    "speed": 3,
+    "unit": "GIS",
+    "position": "d",
+    "nickname": "You're mine"
+}
+Maestro = {
+    "name": "Maestro",
+    "speed": 1,
+    "unit": "GIS",
+    "position": "d",
+    "nickname": "NA"
+}
+operators = [Ash, Thermite, Pulse, Castle, Sledge, Thatcher, Mute, Smoke, Montagne, Twitch, Rook, Doc, IQ, Blitz, Jager,
+Bandit, Frost, Buck, Caveira, Capitao, Hibana, Echo, Glaz, Fuze, Kapkan, Tachanka, Jackal, Mira, Zofia, Ela, Ying, Lesion,
+Lion, Finka, Alibi, Maestro] 
+
+""" Clash,
+Maverick,Vigil, Dokkaebi, ,
+ Blackbeard, Valkyrie]"""
+
+forces = ["FBI", "GIGN", "Spetznaz", "SAS", "GSG9", "BOPE", "JTF2", "CBRN", "GIS", "GEO", "GSTUR"
+        "SAT", "707th SMB (SMB)", "Navy SEALs (SEAL)", "SDU", "GROM"]
+
+
+def choose_side():
+    side = input("\nDo you want to be on Attack or Defense? [a/d]")
+    return side
+
+def choose_unit():
+    print("Here are the special forces in the game:\n")
+    print("\n".join(forces))
+    unit = input("\nWhat special forces/unit would you like to choose? [NAME or IDC]\n")
+    return unit
+
+def choose_speed():
+    print("\nSpeed/Armor guide: \n3 speed means 1 armor\n2 speed means 2 armor\n1 speed means 3 armor")
+    speed = float(input("What speed do you want your operator to be? [number]"))
+    return speed
+
+def operator_choice():
+    side = choose_side()
+    unit = choose_unit().upper()
+    speed = int(choose_speed())
+
+    for i in range(len(operators)):
+        if side == operators[i]["position"] and unit == operators[i]["unit"] and speed == operators[i]["speed"] :
+            print(operators[i]["name"])
+
+operator_choice()
+
